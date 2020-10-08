91a1371939fa81e65424f676b80769f8070a4bf1
aksem62 <aksem62@gmail.com>
2020-10-08 21:13:06 +0300

    add timeout on input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,51 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Seach = () => {
-  const [term, setTerm] = useState("");
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect used");
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
   });
 
   return (
@@ -19,8 +60,9 @@ const Seach = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
 
-export default Seach;
+export default Search;
